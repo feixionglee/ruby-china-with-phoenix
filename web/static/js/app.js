124d7702c17fd8b82883dba4a5256bf2b6d799be
lifeixiong <feixiongli@gmail.com>
2016-04-05 17:01:21 +0800

    try to import jquery and bootstrap

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,8 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+var $ = require("jquery");
+
+console.log($('a'));
+console.log($('a').length);
