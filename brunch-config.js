d3296fd438d829e5d8324d068676a4a31ce6e2a8
lifeixiong <feixiongli@gmail.com>
2016-04-14 15:36:04 +0800

    assets

--- brunch-config.js
+++ brunch-config.js
@@ -6,22 +6,19 @@ exports.config = {
 
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
+
       //
       // To change the order of concatenation of files, explicitly mention here
       // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css",
+      order: {
+        before: [
+          "web/static/vendor/css/boostrap.min.css",
+          "web/static/vendor/css/vars.scss"
+        ]
+      }
     },
     templates: {
       joinTo: "js/app.js"
@@ -65,6 +62,6 @@ exports.config = {
     enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html", "jquery", "bootstrap", "font-awesome"]
+    whitelist: ["phoenix", "phoenix_html", "jquery", "boostrap"]
   }
 };
