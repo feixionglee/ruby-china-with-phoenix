e7f3a1ceaaa989ac77a28b7002b9d918e7484f17
lifeixiong <feixiongli@gmail.com>
2016-05-06 00:53:11 +0800

    show post after create

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -43,28 +43,28 @@ defmodule Elixirer.PostController do
       |> Post.changeset(post_params)
 
     case Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, post} ->
         conn
         |> put_flash(:info, "Post created successfully.")
-        |> redirect(to: post_path(conn, :index))
+        |> redirect(to: post_path(conn, :show, post))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id}, user) do
     post = Repo.get!(Post, id)
     render(conn, "show.html", post: post)
   end
 
-  def edit(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+  def edit(conn, %{"id" => id}, user) do
+    post = Repo.get!(user_posts(user), id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(Post, id)
+  def update(conn, %{"id" => id, "post" => post_params}, user) do
+    post = Repo.get!(user_posts(user), id)
     changeset = Post.changeset(post, post_params)
 
     case Repo.update(changeset) do
@@ -92,6 +92,10 @@ defmodule Elixirer.PostController do
   def action(conn, _) do
     apply(__MODULE__, action_name(conn),
         [conn, conn.params, conn.assigns.current_user])
+  end
+
+  defp user_posts(user) do
+      assoc(user, :posts)
   end
 
   defp load_categories(conn, _) do
