46393c4613f8ad4c638388df7eaa24f6e4f25233
lifeixiong <feixiongli@gmail.com>
2016-06-08 01:43:55 +0800

    fix post location

--- web/static/js/post_form.js
+++ web/static/js/post_form.js
@@ -1,8 +1,7 @@
-import $ from "jquery"
+// import $ from "jquery"
 
-$(function () {
-  window.addEventListener('message', function(event) {
-    // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
+$(function(){
+  window.addEventListener('message', function(event){
     var loc = event.data;
     if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
       console.log('location', loc);
@@ -12,6 +11,38 @@ $(function () {
       delete loc.latlng
       $("#post_cityname").val(loc.cityname);
       $("#post_location").val(JSON.stringify(loc));
+
+      $("#added-location").html("<span id=\"cityname\">"+loc.cityname+"</span><a id=\"remove-location\" href=\"javascript:;\"><i>&times;</i></a>")
+
     }
   }, false);
 });
+
+var PostForm = Backbone.View.extend({
+  el: $("#post-form"),
+
+  events: {
+    "click #remove-location" : "removeLocation"
+  },
+
+  initialize: function () {
+    this.$location = this.$("#added-location");
+    this.$post_cityname = this.$("#post_cityname");
+    this.$post_location = this.$("#post_location");
+  },
+
+  render: function() {
+    return this;
+  },
+
+  removeLocation: function() {
+    if (this.$location) {
+      this.$location.html("");
+    }
+    this.$post_cityname.val("");
+    this.$post_location.val("null");
+  }
+});
+
+
+new PostForm;
