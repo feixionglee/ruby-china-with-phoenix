1405c08344c29146567568995b883df7fa3a08c5
lifeixiong <feixiongli@gmail.com>
2016-06-17 18:04:30 +0800

    qq lbs asyn load

--- web/static/js/post_form.js
+++ web/static/js/post_form.js
@@ -1,6 +1,12 @@
 // import $ from "jquery"
 
 $(function(){
+  $("#locationModal").on("shown.bs.modal", function () {
+    var ditu = "<iframe id='mapPage' width='100%' height='100%' frameborder=0 src='http://apis.map.qq.com/tools/locpicker?search=1&type=1&key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp'></iframe>";
+
+    $('#locationModal .modal-body').html(ditu);
+  })
+
   window.addEventListener('message', function(event){
     var loc = event.data;
     if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
