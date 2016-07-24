2147fa82bb97b92020f65c27a363413498acd06a
lifeixiong <feixiongli@gmail.com>
2016-07-25 07:07:57 +0800

    upgrade phoenix_ecto to 3.0

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Elixirer.Mixfile do
     [{:phoenix, "~> 1.2.0-rc"},
      {:phoenix_pubsub, "~> 1.0.0-rc"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 3.0-rc"},
+     {:phoenix_ecto, "~> 3.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
