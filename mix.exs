4cb19c4fab5b2f4e327a96466ecc4f25f77e5355
lifeixiong <feixiongli@gmail.com>
2016-07-28 16:43:38 +0800

    update phoenix from 1.2.0-rc to 1.2.0

--- mix.exs
+++ mix.exs
@@ -30,8 +30,8 @@ defmodule Elixirer.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0-rc"},
-     {:phoenix_pubsub, "~> 1.0.0-rc"},
+    [{:phoenix, "~> 1.2.0"},
+     {:phoenix_pubsub, "~> 1.0.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:phoenix_html, "~> 2.4"},
